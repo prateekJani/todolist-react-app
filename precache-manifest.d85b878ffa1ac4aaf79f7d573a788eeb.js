4b1d9982f02f7ca51b131c14f31bb996be20ef0c
prateekJani <58832930+prateekJani@users.noreply.github.com>
2020-10-02 20:59:03 -0400

    Updates

---
+++ precache-manifest.d85b878ffa1ac4aaf79f7d573a788eeb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca4c34b60b7352986c2af03f9094478b",
+    "url": "/todolist-react-app/index.html"
+  },
+  {
+    "revision": "850e72326d430bb2fcb7",
+    "url": "/todolist-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f848f7ecd35676bd8ba2",
+    "url": "/todolist-react-app/static/js/2.df0bd9f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist-react-app/static/js/2.df0bd9f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "850e72326d430bb2fcb7",
+    "url": "/todolist-react-app/static/js/main.767704e4.chunk.js"
+  },
+  {
+    "revision": "e9dd23c9c0a0e377545d",
+    "url": "/todolist-react-app/static/js/runtime-main.f3476552.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todolist-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
