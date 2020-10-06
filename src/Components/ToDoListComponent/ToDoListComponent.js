e7d5bc41ab0d709bc3d782a5c3fc7bec21740617
prateekJani <58832930+prateekJani@users.noreply.github.com>
2020-10-05 22:13:49 -0400

    Update ToDoListComponent.js

--- src/Components/ToDoListComponent/ToDoListComponent.js
+++ src/Components/ToDoListComponent/ToDoListComponent.js
@@ -5,14 +5,72 @@ class ToDoListComponent extends React.Component{
     constructor(props){
         super(props);
         this.state = {
+            // defaultText: "",
+            task: "",
             todo: []
         }
     }
 
+    // resetValue(){
+    //     this.setState({defaultText: ""})
+    // }
+
+    checkValidtask(t){
+        if(t!==""){
+            return true
+        }else{
+            return false
+        }
+    }
+
+    _checkTask(e){
+        if(this.checkValidtask(this.state.task) === true){
+            let todo = this.state.todo
+            todo.push(this.state.task)
+            this.setState({ task:"",
+                            todo: todo})
+        }else{
+            alert("Enter a valid task before Submit")
+        }
+
+        e.target.reset(); //To go back to the default textField of the form.
+        e.preventDefault(); //To Stop reloading the page as it is a form.
+    }
+
+    _handleTask(e){
+        const newTask = e.target.value
+        
+        this.setState({task: newTask})
+    }
+
+    displayTodoList(){
+        const items = this.state.todo
+        let listItems = items.map((task, count) => {
+            console.log(task, count);
+            return(
+                <li>
+                    {task}
+                </li>
+            )
+        })
+        return (
+            <ul>
+                {listItems}
+            </ul>
+        )
+    }
+
+
     render(){
         return(
-            <div>
-                <p> ToDoList </p>
+            <div className = "body">
+                <div className="header">
+                    <form onSubmit = {(e)=>this._checkTask(e)}>
+                        <input onChange = {(e)=>this._handleTask(e)} placeholder = "Enter today's task" type = "text" id = "taskField"></input>
+                        <button type = "submit" >Add</button>
+                    </form>
+                </div>
+                {this.displayTodoList()}
             </div>
 
         )
