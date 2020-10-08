03ba26f7d461d3d4d0a12ca099ced50a5cc77123
prateekJani <58832930+prateekJani@users.noreply.github.com>
2020-10-07 23:26:46 -0400

    Update ToDoListComponent.js

--- src/Components/ToDoListComponent/ToDoListComponent.js
+++ src/Components/ToDoListComponent/ToDoListComponent.js
@@ -10,7 +10,7 @@ class ToDoListComponent extends React.Component{
         }
     }
 
-    checkValidtask(t){
+    checkValidtask(t){ //It helps us to see is the task a valid task or not before adding that task into todo list
         if(t!==""){
             return true
         }else{
@@ -18,7 +18,7 @@ class ToDoListComponent extends React.Component{
         }
     }
 
-    _addTask(e){
+    _addTask(e){ //This fuction is called when add button is clicked. Used to add tasks as the name suggests
         if(this.checkValidtask(this.state.task) === true){
             let todo = this.state.todo
             todo.push(this.state.task)
@@ -32,19 +32,19 @@ class ToDoListComponent extends React.Component{
         e.preventDefault(); //To Stop reloading the page as it is a form.
     }
 
-    _handleTask(e){
+    _handleTask(e){ //Takes the input from the text field and stores it into task field of the state defined in the constructor.
         const newTask = e.target.value
         
         this.setState({task: newTask})
     }
 
-    displayTodoList(){
+    displayTodoList(){ //Dispaying all the todo task stored in the todo list on the screen. 
         const items = this.state.todo
         let listItems = items.map((task) => {
             return(
-                <li onClick={() => this._delete(task)}>
+                <li onClick={() => this._delete(task)}> 
                     {task}
-                </li>
+                </li>//onclick fucntion is used to pass the value of that particular list which has to be deleted from the todo list.
             )
         })
         return (
@@ -54,7 +54,7 @@ class ToDoListComponent extends React.Component{
         )
     }
 
-    _clearList(){
+    _clearList(){ //To clear all the tasks that are stored in todo list.
         let todolist = this.state.todo
         if( todolist.length > 0){
             this.setState({task: "", todo: []})
@@ -65,18 +65,18 @@ class ToDoListComponent extends React.Component{
     }
 
 
-    _delete(key){
+    _delete(key){ // to delete the specific task from todo list clicked by the user.  
         let modifiedItems = this.state.todo
         modifiedItems = modifiedItems.filter(function (item) {
             return (item !== key);
-        });
+        });//used filter method to iterate through the list and delete the specific task that is given to the function.
          
         this.setState({
             todo: modifiedItems
         });
     }
 
-    render(){
+    render(){ //bascically acts a main fuction of reactjs.
         return(
             <div className = "body">
                 <div className="header">
