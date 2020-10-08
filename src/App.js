13cfffe25a1e545e4318c4f5fc682aff32257e7a
prateekJani <58832930+prateekJani@users.noreply.github.com>
2020-10-07 23:34:29 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo192.png';
+import logo from './todolist.png';
 import './App.css';
 import ToDoListComponent from './Components/ToDoListComponent/ToDoListComponent'
 
