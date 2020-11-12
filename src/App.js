febe5fdcbef09eecf60462180d3daebc44f3ff81
prateekJani <58832930+prateekJani@users.noreply.github.com>
2020-11-12 05:28:50 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import logo from './todolist.png';
 import './App.css';
-import ToDoListComponent from './Components/ToDoListComponent/ToDoListComponent'
+import Grid from '@material-ui/core/Grid';
+import AppliedComponent from './Components/ToDoListComponent/AppliedComponent'
 
 function App() {
   return (
     <div className = "Outerbody">
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} alt = 'Logo' /> 
-        </header>     
-      </div>  
-      <ToDoListComponent />
+      <h1>Application Tracker</h1>
+      <Grid container spacing={3}>
+        <Grid item xs={3}>
+          <AppliedComponent gridName = 'Applied'/>
+        </Grid>
+        <Grid item xs={3}>
+          <AppliedComponent gridName = 'Interview'/>
+        </Grid>
+        <Grid item xs={3}>
+          <AppliedComponent gridName = 'Accept'/>
+        </Grid>
+        <Grid item xs={3}>
+          <AppliedComponent gridName = 'Reject'/>
+        </Grid>
+      </Grid>
     </div>
     
   );
