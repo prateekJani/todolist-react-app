67773fe58fc38fbb59f5bb50d9973730ae467e3f
prateekJani <58832930+prateekJani@users.noreply.github.com>
2020-11-12 10:40:56 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,22 +3,24 @@ import './App.css';
 import Grid from '@material-ui/core/Grid';
 import AppliedComponent from './Components/ToDoListComponent/AppliedComponent'
 
+
 function App() {
+  
   return (
     <div className = "Outerbody">
       <h1>Application Tracker</h1>
-      <Grid container spacing={3}>
-        <Grid item xs={3}>
-          <AppliedComponent gridName = 'Applied'/>
+      <Grid container spacing={6}>
+        <Grid item xs={3} style={{height: '100vh', boxShadow: "1px 3px 1px #9E9E9E", padding: '6px', backgroundColor: '#99ea9b'}} >
+          <AppliedComponent  gridName = 'Applied'/>
         </Grid>
-        <Grid item xs={3}>
-          <AppliedComponent gridName = 'Interview'/>
+        <Grid item xs={3} style={{height: '100vh', boxShadow: "1px 3px 1px #9E9E9E", padding: '6px', backgroundColor: '#79e27b'}}>
+          <AppliedComponent  gridName = 'Interview'/>
         </Grid>
-        <Grid item xs={3}>
-          <AppliedComponent gridName = 'Accept'/>
+        <Grid item xs={3} style={{height: '100vh', boxShadow: "1px 3px 1px #9E9E9E", padding: '6px', backgroundColor: '#58da5a'}}>
+          <AppliedComponent  gridName = 'Accept'/>
         </Grid>
-        <Grid item xs={3}>
-          <AppliedComponent gridName = 'Reject'/>
+        <Grid item xs={3} style={{height: '100vh', boxShadow: "1px 3px 1px #9E9E9E", padding: '6px', backgroundColor: '#f1856a'}}>
+          <AppliedComponent  gridName = 'Reject'/>
         </Grid>
       </Grid>
     </div>
