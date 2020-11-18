57dd3c383c62bd88e2dd66dcc4cdfec5d84d11df
prateekJani <58832930+prateekJani@users.noreply.github.com>
2020-11-18 18:11:33 -0500

    Update Homepage.js

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -4,27 +4,34 @@ import AppliedComponent from '../Components/AppliedComponent'
 
 const statuses = [
     {
-        name: 'Applied'
+        status: 'Applied',
+        cardColor: '#99ea9b'
     },
     {
-        name: 'Interview'
+        status: 'Interview',
+        cardColor: '#99ea9b'
     },
     {
-        name: 'Accept'
+        status: 'Accept',
+        cardColor: '#99ea9b'
     },
     {
-        name: 'Reject'
+        status: 'Reject',
+        cardColor: '#99ea9b'
     },    
 ]
 
+
+
 function homepage() {
+
     return (
         <div>
             <Grid container spacing={2}>
                 {statuses.map(s =>{
                     return (
                         <Grid item xs={3} style={{height: '100vh', boxShadow: "1px 3px 1px #9E9E9E", padding: '6px', backgroundColor: '#f8f8f8'}} >
-                            <AppliedComponent  gridName = {s.name} />
+                            <AppliedComponent  status = {s.status} />
                         </Grid>
                     )
                 })}
