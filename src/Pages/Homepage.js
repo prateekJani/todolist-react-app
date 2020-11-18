0c06fd019d2d8ed95ec3d74ca2779b4091f95fc9
prateekJani <58832930+prateekJani@users.noreply.github.com>
2020-11-18 15:31:44 -0500

    Update Homepage.js

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -2,22 +2,32 @@ import React from 'react'
 import Grid from '@material-ui/core/Grid';
 import AppliedComponent from '../Components/AppliedComponent'
 
+const statuses = [
+    {
+        name: 'Applied'
+    },
+    {
+        name: 'Interview'
+    },
+    {
+        name: 'Accept'
+    },
+    {
+        name: 'Reject'
+    },    
+]
+
 function homepage() {
     return (
         <div>
             <Grid container spacing={2}>
-                <Grid item xs={3} style={{height: '100vh', boxShadow: "1px 3px 1px #9E9E9E", padding: '6px', backgroundColor: '#f8f8f8'}} >
-                    <AppliedComponent  gridName = 'Applied'/>
-                </Grid>
-                <Grid item xs={3} style={{height: '100vh', boxShadow: "1px 3px 1px #9E9E9E", padding: '6px', backgroundColor: '#f8f8f8'}}>
-                    <AppliedComponent  gridName = 'Interview'/>
-                </Grid>
-                <Grid item xs={3} style={{height: '100vh', boxShadow: "1px 3px 1px #9E9E9E", padding: '6px', backgroundColor: '#f8f8f8'}}>
-                    <AppliedComponent  gridName = 'Accept'/>
-                </Grid>
-                <Grid item xs={3} style={{height: '100vh', boxShadow: "1px 3px 1px #9E9E9E", padding: '6px', backgroundColor: '#f8f8f8'}}>
-                    <AppliedComponent  gridName = 'Reject'/>
-                </Grid>
+                {statuses.map(s =>{
+                    return (
+                        <Grid item xs={3} style={{height: '100vh', boxShadow: "1px 3px 1px #9E9E9E", padding: '6px', backgroundColor: '#f8f8f8'}} >
+                            <AppliedComponent  gridName = {s.name} />
+                        </Grid>
+                    )
+                })}
             </Grid>
         </div>
     )
